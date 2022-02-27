38392497283f54629e23900c7bc30ee25d80bd1f
Priya162001 <100488245+Priya162001@users.noreply.github.com>
2022-02-27 18:37:05 +0530

    Add files via upload

---
+++ BasicCalculatorOperation.java
@@ -0,0 +1,21 @@
+
+
+//write a java program to perform basic calculator operation
+class Demo{ 
+
+	public static void main(String [] args){
+
+		//Arithmatic operater
+		int x= 20;
+		int y= 10;
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+
+		//Relational Operater
+		System.out.println(x<y);
+		System.out.println(x>y);
+	}
+}
